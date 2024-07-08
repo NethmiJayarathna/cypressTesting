ea157ecd868ac52512a3d85ca37cb843e4d5887b
NethmiJayarathna <it21208812@my.sliit.lk>
2024-07-08 09:02:41 +0530

    Add Changes

---
+++ cypress/e2e/Alerts.cy.js
@@ -0,0 +1,11 @@
+describe('Alerts', ()=>{
+
+    // Javascript Alert: It will have some text and an 'OK' button
+
+    // Javascript Confirm Alert: It will have some text with 'OK' and 'Cancle' button
+
+    // Javascript Prompt Alert: It will have some text with a text box for user input along with 'OK'
+
+    // Authenticated Alert
+
+})
